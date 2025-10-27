ca2f09453b82b01949c99a0afce467e1d954fe14
zalmyr <zalmyr@gmail.com>
2025-10-26 22:07:57 -0400

    updated dockerfile

--- lxmf_ha_conversations/run.sh
+++ lxmf_ha_conversations/run.sh
@@ -1,15 +1,23 @@
 #!/usr/bin/with-contenv bashio
 set -e
 
+# Activate the virtual environment if available
+if [ -f "/opt/lxmf/bin/activate" ]; then
+    bashio::log.info "Activating LXMF virtual environment..."
+    source /opt/lxmf/bin/activate
+else
+    bashio::log.warning "Virtual environment not found, falling back to system Python."
+fi
+
 HA_URL=$(bashio::config 'ha_url')
 HA_TOKEN=$(bashio::config 'ha_token')
 ADMIN_HASHES=$(bashio::config 'admin_hashes')
 BOT_NAME=$(bashio::config 'bot_name')
 
 bashio::log.info "Starting LXMF ↔ HA Conversations bridge..."
-bashio::log.info "Home Assistant URL: ${HA_URL}"
-bashio::log.info "Bot Name: ${BOT_NAME}"
+bashio::log.info "Connecting to $HA_URL as $BOT_NAME"
 
+# Run the LXMF bot
 lxmf-ha-conversations \
     --ha-url "$HA_URL" \
     --ha-token "$HA_TOKEN" \
